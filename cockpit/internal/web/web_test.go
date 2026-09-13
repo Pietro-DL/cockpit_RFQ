583b1b41548488ea108e00f1d1bf7b5b97a824f7
Pietro-DL <pietro.spinozzi02@gmail.com>
2026-09-13 13:16:57 +0100

    Fix sintassi template frammenti.html e aggiunto test unitario web_test.go

---
+++ cockpit/internal/web/web_test.go
@@ -0,0 +1,25 @@
+package web_test
+
+import (
+	"io/fs"
+	"testing"
+
+	risorse "promatec/cockpit"
+	"promatec/cockpit/internal/web"
+)
+
+func TestTemplates(t *testing.T) {
+	templ, err := fs.Sub(risorse.FS, "web/templates")
+	if err != nil {
+		t.Fatalf("sub fs: %v", err)
+	}
+	static, err := fs.Sub(risorse.FS, "web/static")
+	if err != nil {
+		t.Fatalf("sub fs: %v", err)
+	}
+
+	s := &web.Server{Templ: templ, Static: static}
+	if err := s.Init(); err != nil {
+		t.Fatalf("Init templates fallito: %v", err)
+	}
+}
